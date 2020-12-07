d0f957bb2e77b4ada8eb448ad2374bb3a56b9cf0
Bordeanu Lucia <bordeanu_lucia@yahoo.com>
2020-12-07 19:17:45 +0200

    sendMessage

--- src/main/java/SocialNetwork/domain/MessageDTO.java
+++ src/main/java/SocialNetwork/domain/MessageDTO.java
@@ -1,11 +1,17 @@
 package SocialNetwork.domain;
 
 public class MessageDTO {
+    private Long id;
     private Utilizator user;
     private String text;
     private String reply;
 
-    public MessageDTO(Utilizator user, String text, String reply) {
+    public Long getId() {
+        return id;
+    }
+
+    public MessageDTO(Long id, Utilizator user, String text, String reply) {
+        this.id = id;
         this.user = user;
         this.text = text;
         this.reply = reply;
